c42f3535531462a9d624cd5aa8ab8cea6d9f05c3
Aidan Lincicum <aidanlincicum@gmail.com>
2019-02-14 19:05:45 -0600

    Finished the code for the back end

--- Back End/PythonLib/prepopulate.php
+++ Back End/PythonLib/prepopulate.php
@@ -49,7 +49,7 @@
       								<div class="row">
       									<div class="small-12 columns">
       										<label>Match Number: *
-											  <input  name="MatchNumber" class="form-control" type="number" placeholder="1" value="<?php $_GET['match']; ?>" required="" readonly/>
+											  <input  name="MatchNumber" class="form-control" type="number" placeholder="1" value="<?php echo $_GET['match']; ?>" required="" readonly/>
 											</label>
 										</div>
 									</div>
@@ -239,6 +239,7 @@
                                         <input name="ClimbAssist" id="ClimbAssist" type="checkbox"><label for="ClimbAssist">Climb Assist?</label> -->
                                     </div>
 								</fieldset>
+								</div>
       						</div>
                             <div class="row">
                             <div class="large-12 columns">
