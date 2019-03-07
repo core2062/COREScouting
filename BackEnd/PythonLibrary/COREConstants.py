7a07d1aededce8b6e61247d830ea43f5f81df1e0
Aidan Lincicum <aidanlincicum@gmail.com>
2019-03-07 09:42:49 -0600

    Added midwest practice schedule

--- BackEnd/PythonLibrary/COREConstants.py
+++ BackEnd/PythonLibrary/COREConstants.py
@@ -14,7 +14,7 @@ General Team Info (no dependency)
 
 TEAM_NUMBER = 2062
 
-COMPETITION_NAME = 'week0Test'
+COMPETITION_NAME = 'midwest_practice'
 
 """===========================================
 Scout HTML input field names (form dependency)
