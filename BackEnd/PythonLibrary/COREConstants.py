a589342ea27dea23ceaa1c311eca20942214ab07
Aidan Lincicum <aidanlincicum@gmail.com>
2019-03-08 08:07:02 -0600

    Midwest Quals

--- BackEnd/PythonLibrary/COREConstants.py
+++ BackEnd/PythonLibrary/COREConstants.py
@@ -14,7 +14,7 @@ General Team Info (no dependency)
 
 TEAM_NUMBER = 2062
 
-COMPETITION_NAME = 'midwest_practice'
+COMPETITION_NAME = 'midwest_quals'
 
 """===========================================
 Scout HTML input field names (form dependency)
