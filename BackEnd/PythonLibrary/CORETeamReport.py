1088cf7113b8eb82f700584c93dd36449d5e2985
Aidan Lincicum <aidanlincicum@gmail.com>
2019-03-16 11:46:49 -0500

    Took out str

--- BackEnd/PythonLibrary/CORETeamReport.py
+++ BackEnd/PythonLibrary/CORETeamReport.py
@@ -32,11 +32,11 @@ for dictionary_key in COREDependencies.COREConstants.RANK_ONLY_HEADERS:
 print('</tr>')
 print('<tr>')
 for dictionary_key in COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS:
-    print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
+    print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
 for dictionary_key in COREDependencies.COREConstants.MATCH_HEADERS:
-    print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
+    print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
 for dictionary_key in COREDependencies.COREConstants.RANK_ONLY_HEADERS:
-    print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
+    print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
 print('</tr>')
 print('</table>')
 
