db2c309d9283c28093d363aa7bd53687564dff6f
Aidan Lincicum <aidanlincicum@gmail.com>
2019-03-16 12:09:19 -0500

    More unicode fixes

--- BackEnd/PythonLibrary/CORETeamReport.py
+++ BackEnd/PythonLibrary/CORETeamReport.py
@@ -35,17 +35,17 @@ for dictionary_key in COREDependencies.COREConstants.RANK_ONLY_HEADERS:
 print('</tr>')
 print('<tr>')
 for dictionary_key in COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS:
-    if(type(calculated_team_data.teamdata[dictionary_key]) == str):
+    if(type(calculated_team_data.team_data[dictionary_key]) == str):
         print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
     else: 
         print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
 for dictionary_key in COREDependencies.COREConstants.MATCH_HEADERS:
-    if(type(calculated_team_data.teamdata[dictionary_key]) == str):
+    if(type(calculated_team_data.team_data[dictionary_key]) == str):
         print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
     else: 
         print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
 for dictionary_key in COREDependencies.COREConstants.RANK_ONLY_HEADERS:
-    if(type(calculated_team_data.teamdata[dictionary_key]) == str):
+    if(type(calculated_team_data.team_data[dictionary_key]) == str):
         print('<td>' + (calculated_team_data.team_data[dictionary_key]) + '</td>')
     else: 
         print('<td>' + str(calculated_team_data.team_data[dictionary_key]) + '</td>')
