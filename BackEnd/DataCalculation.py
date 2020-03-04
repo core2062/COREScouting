e3fcdef334ac864befdbc6ad85657c309e6764ff
Aidan Lincicum <aidanlincicum@gmail.com>
2020-03-03 18:05:56 -0600

    Changes made for the 2020 season

--- BackEnd/DataCalculation.py
+++ BackEnd/DataCalculation.py
@@ -17,41 +17,21 @@ class TeamData(CORETeamData.Team):
         self.team_data[COREDependencies.COREConstants.REPORT_HEADER[3]] = self.list_all_results(COREDependencies.COREConstants.TEXT_NAMES[0])"""
         # Total amount of matches played
         MatchesPlayed = self.num_data_entries('MatchNumber')
-        #Average High Hatch 
-        AverageHighHatch = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[1])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[0]] = AverageHighHatch
-        #Average Medium Hatch
-        AverageMediumHatch = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[2])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[1]] = AverageMediumHatch
-        #Average Low Hatch
-        AverageLowHatch = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[3])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[2]]  = AverageLowHatch
-        #Average High Cargo
-        AverageHighCargo = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[4])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[3]] = AverageHighCargo
-        #Average Medium Cargo
-        AverageMediumCargo = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[5])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[4]] = AverageMediumCargo
-        #Average Low Cargo
-        AverageLowCargo = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[6])
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[5]] = AverageLowCargo
-        #Average Cargo
-        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[2]] = AverageHighCargo + AverageMediumCargo + AverageLowCargo
-        #Average Hatches
-        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[3]] = AverageHighHatch + AverageLowHatch + AverageMediumHatch
-        #Climbs
-        TotalNoClimbs = self.times_key_exists_in_category(COREDependencies.COREConstants.RADIO_NAMES[0], 'NoClimb')
-        Total1stLevelClimbs = self.times_key_exists_in_category(COREDependencies.COREConstants.RADIO_NAMES[0], '1stLevel')
-        Total2ndLevelClimbs = self.times_key_exists_in_category(COREDependencies.COREConstants.RADIO_NAMES[0], '2ndLevel')
-        Total3rdLevelClimbs = self.times_key_exists_in_category(COREDependencies.COREConstants.RADIO_NAMES[0], '3rdLevel')
-        TotalClimbFails = self.times_key_exists_in_category(COREDependencies.COREConstants.RADIO_NAMES[0], 'ClimbFail')
-        self.team_data[COREDependencies.COREConstants.MATCH_HEADERS[0]] = str(Total1stLevelClimbs) + ' : ' + str(Total2ndLevelClimbs) + ' : ' + str(Total3rdLevelClimbs) + ' : ' + str(TotalClimbFails) + ' : ' + str(TotalNoClimbs)
-        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[6]] = ((Total3rdLevelClimbs) / MatchesPlayed)
         # Comments
         self.team_data[COREDependencies.COREConstants.MATCH_HEADERS[1]] = self.list_all_results(COREDependencies.COREConstants.TEXT_NAMES[1])
-        # Average Hatches Auton
-        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[0]]=self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[7])
-        #Average Cargo Auton
-        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[1]]=self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[8])
-        # Climb Assist
-        # self.team_data[COREDependencies.COREConstants.MATCH_HEADERS[5]] = self.times_key_exists_in_category('ClimbAssist', 'ON') / MatchesPlayed
+        # Average # of Balls in High Goal
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[0]] = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[1])
+        # Average # of Balls in Low Goal
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[1]] = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[2])
+        # Average # of Balls in High Goal Auton
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[2]] = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[3])
+        # Average # of Balls in Low Goal Auton
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[3]] = self.avg_data(COREDependencies.COREConstants.NUMBER_NAMES[4])
+        # Percent Climbed in Matches
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[4]] = self.times_key_exists_in_category('Climb', 'ON') / MatchesPlayed
+        # Percent Rotate Control Panel 1
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[5]] = self.times_key_exists_in_category('ControlPanel1', 'ON') / MatchesPlayed
+        # Percent Rotate Control Panel 2
+        self.team_data[COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS[6]] = self.times_key_exists_in_category('ControlPanel2', 'ON') / MatchesPlayed
+        # Percent Move in Autonomous
+        self.team_data[COREDependencies.COREConstants.RANK_ONLY_HEADERS[0]] = self.times_key_exists_in_category('AutoStart', 'ON') / MatchesPlayed
