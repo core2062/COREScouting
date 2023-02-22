f5956cdcda7d1bcec06b0cd452b8be62776b7efa
COREProgrammers <core2062programmers@googlegroups.com>
2023-02-21 19:08:44 -0600

    Updated more css and GUI

--- BackEnd/PythonLibrary/CORETeamReport.py
+++ BackEnd/PythonLibrary/CORETeamReport.py
@@ -22,8 +22,7 @@ calculated_team_data.populate_data()
 COREDependencies.framework_begining()
 print('<body link="##000000">')
 print(str(team_number))
-print('<img src="http://2062scouting.imgix.net/' + str(team_number) +
-      '.jpg?h=200" alt="Team Image Not Available">')
+# print('<img src="http://2062scouting.imgix.net/' + str(team_number) + '.jpg?h=200" alt="Team Image Not Available">') <-- Images still don't work
 print('<table>')
 print('<tr>')
 for dictionary_key in COREDependencies.COREConstants.RANK_AND_MATCH_HEADERS:
